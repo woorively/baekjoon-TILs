08faa8ccf3e395fe0934bd3e3a1668f572d99883
woorively <87566256+woorively@users.noreply.github.com>
2024-09-01 13:17:34 +0900

    [Silver III] Title: N과 M (2), Time: 188 ms, Memory: 18356 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	static int n, m;
-	static ArrayList<Integer> arr;
+	static int[] arr;
 	static boolean[] selected;
 	
 	public static void main(String[] args) {
@@ -11,47 +10,29 @@ public class Main {
 		n = s.nextInt();
 		m = s.nextInt();
 		
-		arr = new ArrayList<>();
+		arr = new int[m];
 		selected = new boolean[n+1];
 		
 		// 1부터 n까지의 자연수 중에서 중복 없이 m개를 고른 수열
-		// 순열 
-		perm(0);
+		// 오름차
+		recur(1, 0);
 	}
 	
-	static void perm(int cnt) {
+	static void recur(int idx, int cnt) {
 		// 기저조건
-		// m개를 뽑으면 수열 출력하기  
 		if (cnt == m) {
-			if (!check()) return;
 			for (int i=0; i<m; i++) {
-				System.out.print(arr.get(i) + " ");
+				System.out.print(arr[i]+ " ");
 			}
 			System.out.println();
 			return;
 		}
 		
-		// 숫자를 뽑을 것인데 
-		for (int i=1; i<=n; i++) {
-			if (selected[i]) continue;
-			
-			arr.add(i);
-			selected[i] = true;
-			// 하나 더 뽑기 
-			perm(cnt+1);
-			// 뽑은 것은 다시 빼주기 
-			arr.remove(arr.size()-1);
-			selected[i] = false;
-			
+		
+		for (int i=idx; i<=n; i++) {
+			arr[cnt] = i;
+			recur(i+1, cnt+1);
 		}
-	}
-	
-	static boolean check() {
-		for (int i=1; i<m; i++) {
-			if (arr.get(i-1) > arr.get(i))
-				return false;
-		}
-		return true;
 	}
 
 }
