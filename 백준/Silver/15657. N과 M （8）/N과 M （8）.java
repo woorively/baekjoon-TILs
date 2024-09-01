dbe264048039da19f5b31b886a4ee606997a49f6
woorively <87566256+woorively@users.noreply.github.com>
2024-09-01 14:51:08 +0900

    [Silver III] Title: N과 M (8), Time: 212 ms, Memory: 21052 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int n, m;
+	static int[] arr, result;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) {
+		sb = new StringBuilder();
+		Scanner s = new Scanner(System.in);
+		n = s.nextInt();
+		m = s.nextInt();
+		
+		arr = new int[n];
+		result = new int[m];
+		
+		for (int i=0; i<n; i++) {
+			arr[i] = s.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		recur(0, 0);
+		System.out.println(sb);
+	}
+	
+	static void recur(int idx, int depth) {
+		if (depth == m) {
+			for (int num: result) {
+				sb.append(num).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		// 1부터 n까지 뽑는데,
+		for (int i=idx; i<n; i++) {
+			result[depth] = arr[i];
+			recur(i, depth+1);
+		}
+	}
+
+}
