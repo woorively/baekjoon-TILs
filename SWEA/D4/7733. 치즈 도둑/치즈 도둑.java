815af623ae877b83e9b561a3e141393b3de814e8
woorively <87566256+woorively@users.noreply.github.com>
2024-09-28 16:54:55 +0900

    [D4] Title: 치즈 도둑, Time: 497 ms, Memory: 108,724 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,63 +1,69 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
 	static int n, day;
 	static int[][] map;
 	static boolean[][] visited;
+	static int[] drs = {1, -1, 0, 0};
+	static int[] dcs = {0, 0, 1, -1};
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		for (int tc=1; tc<=T; tc++) {
-			n = sc.nextInt();
-			map = new int[n][n];
-			for (int i=0; i<n; i++) {
-				for (int j=0; j<n; j++) {
-					map[i][j] = sc.nextInt();
-				}
-			}
-			
-			int ans = 1;
-			for (day=1; day<=100; day++) {
-				int r = 0, c = 0;
-				visited = new boolean[n][n];
-				int cnt = 0;
-				
-				for (int i=0; i<n; i++) {
-					for (int j=0; j<n; j++) {
-						if (visited[i][j] || map[i][j] <= day) 
-							continue;
-						dfs(i, j);
-						cnt++;
-					}
-					
-					ans = Math.max(ans, cnt);
-				}
-			}
-			
-			
-			System.out.println("#" + tc + " " + ans);
-		}
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int tc=1; tc<=T; tc++) {
+        	sb.append("#").append(tc).append(" ");
+        	n = Integer.parseInt(br.readLine());
+        	map = new int[n][n];
+        	
+        	for (int i=0; i<n; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		for (int j=0; j<n; j++) {
+        			map[i][j] = Integer.parseInt(st.nextToken());
+        		}
+        	}
+        	
+        	int ans = 1;
+        	for (day=1; day<=100; day++) {
+        		int cnt = 0;
+        		visited = new boolean[n][n];
+        		for (int i=0; i<n; i++) {
+        			for (int j=0; j<n; j++) {
+        				if (visited[i][j] || map[i][j] <= day) continue;
+        				dfs(i, j);
+        				cnt++;
+        			}
+        		}
+        		ans = Math.max(ans, cnt);
+        	}
+        	
+        	sb.append(ans).append("\n");
+        }
+        
+        System.out.println(sb);
 	}
 	
 	static void dfs(int r, int c) {
-		int[] dr = {1, -1, 0, 0};
-		int[] dc = {0, 0, 1, -1};
+		visited[r][c] = true;
 		
 		for (int d=0; d<4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
+			int nr = r + drs[d];
+			int nc = c + dcs[d];
 			
 			if (canGo(nr, nc)) {
-				visited[nr][nc] = true;
 				dfs(nr, nc);
 			}
 		}
 	}
 	
 	static boolean canGo(int r, int c) {
-		return 0 <= r && r < n && 0 <= c && c < n 
-				&& !visited[r][c] && map[r][c] > day;
+		return 0 <= r && r < n && 0 <= c && c < n
+				&& map[r][c] > day && !visited[r][c];
 	}
+
 }
