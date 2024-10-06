41af4b885d57f4aeeb27f1501e6d6902b5a79ced
woorively <87566256+woorively@users.noreply.github.com>
2024-10-06 20:32:16 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 127 ms, Memory: 18,628 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,51 +1,62 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Solution {
-	static int ans;
-	static int[][] maze;
-	static int[] drs = {1, -1, 0, 0};
-	static int[] dcs = {0, 0, -1, 1};
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		for (int tc=1; tc<=10; tc++) {
-			int T = sc.nextInt();
-			ans = 0;
-			maze = new int[16][16];
-			
-			for (int i=0; i<16; i++) {
-				String str = sc.next();
-				for (int j=0; j<16; j++) {
-					maze[i][j] = str.charAt(j) - '0';
-				}
-			}
-			
-			maze[1][1] = 1;
-			dfs(1, 1);
-			
-			System.out.println("#" + T + " " + ans);
-		}
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        for (int tc=1; tc<=10; tc++) {
+        	int T = Integer.parseInt(br.readLine());
+        	
+        	int[][] map = new int[16][16];
+        	boolean[][] visited = new boolean[16][16];
+        	for (int i=0; i<16; i++) {
+        		String str = br.readLine();
+        		for (int j=0; j<16; j++) {
+        			map[i][j] = str.charAt(j)-'0';
+        		}
+        	}
+        	
+        	int[] drs = {1, -1, 0, 0};
+        	int[] dcs = {0, 0, 1, -1};
+        	
+        	visited[1][1] = true;
+        	Queue<int[]> q = new LinkedList<>();
+        	
+        	int ans = 0;
+        	q.add(new int[] {1, 1});
+        	out : while(!q.isEmpty()) {
+        		int[] curr = q.poll();
+        		
+        		int r = curr[0];
+        		int c = curr[1];
+        		
+        		for (int d=0; d<4; d++) {
+        			int nr = r + drs[d];
+        			int nc = c + dcs[d];
+        			
+        			if (0 <= nr && nr < 16 && 0 <= nc && nc < 16
+        					&& map[nr][nc] != 1 && !visited[nr][nc]) {
+        				if (map[nr][nc] == 3) {
+        					ans = 1;
+        					break out;
+        				}
+        				visited[nr][nc] = true;
+        				q.add(new int[] {nr, nc});
+        				
+        			}
+        		}
+        	}
+            
+        	sb.append("#").append(T).append(" ").append(ans).append("\n");
+        }
+        
+        System.out.println(sb);
 	}
-	
-	static void dfs(int r, int c) {
-		
-		for (int d=0; d<4; d++) {
-			int nr = r + drs[d];
-			int nc = c + dcs[d];
-			
-			if (inRange(nr, nc) && maze[nr][nc] != 1) {
-				if (maze[nr][nc] == 3) {
-					ans = 1;
-					return;
-				}
-				maze[nr][nc] = 1;
-				dfs(nr, nc);
-			}
-		}
-	}
-	
-	static boolean inRange(int r, int c) {
-		return 0 <= r && r < 16 && 0 <= c && c < 16;
-	}
+
 }
